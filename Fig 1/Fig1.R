df35ba28257cb72a6871f4c7f5cc9fdc97b38abb
Tatianaproboste <38739705+Tatianaproboste@users.noreply.github.com>
2019-07-04 13:54:14 +1000

    Fig 1

--- Fig 1/Fig1.R
+++ Fig 1/Fig1.R
@@ -2,21 +2,51 @@
 library(dplyr)
 library(ggplot2)
 
+load("./test_results/test.unweigthed.v3.rda")
+test <-test.unweigthed.v3
+  
+  plot.data <- data.frame(Ecoli.cutoff = as.factor(test$ecoli.cutoff.raw),
+                        Host.cutoff = test$host.cutoff.raw,
+                        index = test$host.index.raw,
+                        Coef = test$degree.coef.raw %>%
+                          filter(Parameter=='Ecoli.degree'),
+                        Coef.Year.2015 = test$degree.coef.raw %>%
+                          filter(Parameter=='Year.2015'),
+                        Coef.betweeness = test$betweeness.coef.raw %>%
+                          filter(Parameter=='Ecoli.betweenness'),
+                        Coef.betweeness.Year.2015 = test$betweeness.coef.raw %>%
+                          filter(Parameter=='Ecoli.betweenness*Year.2015'),
+                        Coef.eigencent = test$eigencent.coef.raw %>%
+                          filter(Parameter=='Ecoli.evcent'),
+                        Coef.eigencent.Year.2015 = test$eigencent.coef.raw %>%
+                          filter(Parameter=='Year.2015'),
+                        Coef.degree.Year.2015 = test$degree.coef.raw %>%
+                          filter(Parameter=='Ecoli.degree*Year.2015'))
 
-#load the data base
-data.plot <- read.csv ("./database_Fig1.csv")
 
-#select the method - interaction 
-type.method.interaction <- interaction(data.plot$Ecoli.cutoff, data.plot$index)
+type.method.interaction <- interaction(plot.data$Ecoli.cutoff, plot.data$index)
 
+# Compute the number of types and methods
+nb.cut.off <- nlevels(plot.data$Ecoli.cutoff)
+nb.index <- nlevels(plot.data$index)
 
+#camparison betweeen social index   
+cols <- c('#A3245B', '#597D79','#E7A582', '#ACC66B')
 
-fig1 <- ggplot(data = data.plot, aes(x = Host.cutoff, y = Coef.Estimate, colour =type.method.interaction, shape=type.method.interaction)) + 
+fig1 <- ggplot(data = plot.data, aes(x = Host.cutoff, y = Coef.Estimate, colour =type.method.interaction, shape=type.method.interaction)) + 
   geom_point (size=1.5)+
   theme_classic() +
   scale_shape_manual("Similarity cut-off & Index", values = c(17,16,17,16)) +
-  scale_colour_discrete("Similarity cut-off & Index") +
+  scale_colour_manual("Similarity cut-off & Index", values = cols) +
   labs(x= "Host association thresholds", y="Regression coefficient") +
-  geom_smooth (method = "loess", size = 0.5, se=F, span=0.5)
+  geom_smooth (method = "loess", size = 0.75, se=F, span=0.6)
 
+plot(fig1)
+
+fig1 <- ggplot(data = plot.data, aes(x = Host.cutoff, y = Coef.Estimate, colour =type.method.interaction, shape=type.method.interaction)) + 
+  theme_classic() +
+  scale_shape_manual("Similarity cut-off & Index", values = c(17,16,17,16)) +
+  scale_colour_manual("Similarity cut-off & Index", values = cols) +
+  labs(x= "Host association thresholds", y="Regression coefficient") +
+  geom_smooth(method = lm, formula = y ~ splines::bs(x, 5), se = FALSE)
 plot(fig1)
